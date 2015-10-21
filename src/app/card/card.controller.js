7191bce5d191f5d68b131925f977d5e186c56b9d
Hom <yanzhihong23@qq.com>
2015-10-21 11:08:05 +0800

    check pay password after active credit

--- src/app/card/card.controller.js
+++ src/app/card/card.controller.js
@@ -167,7 +167,6 @@
           // save bind card success log
           saveActionLog(true);
           activeCredit();
-          payPasswordCheck();
         } else {
           utils.alert({
             title: 'sorry，还款卡绑定失败！',
@@ -260,8 +259,10 @@
       }).success(function(data) {
         if(+data.result === 1) {
           $log.info('active payment success');
+          payPasswordCheck();
         } else {
           $log.info('active payment failed');
+          utils.alert({content: data.message});
         }
       })
     };
