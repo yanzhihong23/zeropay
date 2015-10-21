bde06f7b2263e19cdc24b91f7eb91745077c5ab6
Hom <yanzhihong23@qq.com>
2015-10-21 11:08:41 +0800

    Revert "check pay password after active credit"
    This reverts commit 7191bce5d191f5d68b131925f977d5e186c56b9d.

--- src/app/card/card.controller.js
+++ src/app/card/card.controller.js
@@ -167,6 +167,7 @@
           // save bind card success log
           saveActionLog(true);
           activeCredit();
+          payPasswordCheck();
         } else {
           utils.alert({
             title: 'sorry，还款卡绑定失败！',
@@ -259,10 +260,8 @@
       }).success(function(data) {
         if(+data.result === 1) {
           $log.info('active payment success');
-          payPasswordCheck();
         } else {
           $log.info('active payment failed');
-          utils.alert({content: data.message});
         }
       })
     };
